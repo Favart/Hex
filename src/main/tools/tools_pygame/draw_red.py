f714bd44598d823d718aa083e001bd19a277a74a
Arthur Favereaux <pro.arthurfavereaux@gmail.com>
2022-02-07 09:29:33 +0100

    retrait du dossier pygame

--- src/main/tools/tools_pygame/draw_red.py
+++
@@ -1,9 +0,0 @@
-import pygame
-from math import sqrt,pi,cos,sin
-
-def draw_red(surface,pos):
-    x = pos[0]
-    y = pos[1]
-    pygame.draw.polygon(surface=surface
-                        , color=(255,0,0)
-                        , points=[(120+(x+y-2)*80+50*cos(pi*i/3),500+(x-y)*46+50*sin(pi*i/3)) for i in range(6)])
